90d618d513edb9bf729365df85b1367c16ca7190
ChuksBen <ibechukwuebuka28@gmail.com>
2022-09-14 16:41:12 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _islower - single line
+ * @c: contains values to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
