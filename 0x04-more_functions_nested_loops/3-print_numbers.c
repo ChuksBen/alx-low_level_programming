fd0070af479f97807c686bad3d2b3413a8ddf2a0
ChuksBen <ibechukwuebuka28@gmail.com>
2022-09-15 18:41:52 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,12 @@
  * print_numbers - prints the numbers 0 to 9 followed by a new line
  * Return: Always 0.
  */
+
 void print_numbers(void);
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 48; c < 58; c++)
 	{
 		putchar(c);
 	}
