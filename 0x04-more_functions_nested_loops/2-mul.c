fabed756d4dc94b93f9664c7bf76b1774a5f2f90
ChuksBen <ibechukwuebuka28@gmail.com>
2022-09-15 17:08:59 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+s (11 sloc)  178 Bytes
+
+
+
+#included "main.h"
+
+/**
+ * mul - multiplication of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
